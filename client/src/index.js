a3dfb8f508439039936a581c02b69177258e7ff6
alec <kaltschmidalec@gmail.com>
2021-10-01 12:14:33 -0700

    test

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>test push</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
