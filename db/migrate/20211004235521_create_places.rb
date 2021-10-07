27745d09f2dcccccedf3c5aa3f31f23f30856a61
Michael <mkenton2020@icloud.com>
2021-10-07 11:40:08 -0500

    update schema lat-long and bucketlist

--- db/migrate/20211004235521_create_places.rb
+++ db/migrate/20211004235521_create_places.rb
@@ -6,16 +6,14 @@ class CreatePlaces < ActiveRecord::Migration[6.1]
       t.integer :user_id
       t.text :description
       t.string :image_url
-      t.integer :location_id
       t.integer :check_ins 
       t.boolean :bucket_list 
       t.string :artist_name
       t.boolean :verified
       t.string :date_uploaded 
-      t.integer :longitude 
-      t.integer :latitude
+      t.string :longitude 
+      t.string :latitude
       t.string :type 
-      
 
 
       t.timestamps
